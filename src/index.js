c659c24d4f07f112f97d1808012fae41883e1d29
Stanislav Taran <stastaransa1nt@gmail.com>
2020-12-27 21:50:15 +0200

    a little refactoingr

--- src/index.js
+++ src/index.js
@@ -1,35 +1,37 @@
 import './styles.css';
 
 // REFs
-const startButtonElem = document.getElementById('start-button');
-const newGameButtonElem = document.getElementById('reset-button');
-const scoreListElem = document.getElementById('score-list');
-const squaresListElem = document.getElementById('squares-list');
-const timerValueElem = document.getElementById('time');
-const scoreValueElem = document.getElementById('score');
-const formScoreElem = document.getElementById('form-score');
-const resultFormElem = document.getElementById('result-form');
-const modalElem = document.getElementById('modal');
+const refs = {
+  startButtonElem: document.getElementById('start-button'),
+  newGameButtonElem: document.getElementById('reset-button'),
+  scoreListElem: document.getElementById('score-list'),
+  squaresListElem: document.getElementById('squares-list'),
+  timerValueElem: document.getElementById('time'),
+  scoreValueElem: document.getElementById('score'),
+  formScoreElem: document.getElementById('form-score'),
+  resultFormElem: document.getElementById('result-form'),
+  modalElem: document.getElementById('modal'),
+};
 
 //CONSTANTS
- const SQUARES_QTY = 70;
- const INITIAL_TIMER_VALUE = 10;
- const SQUARES_QTY_ON_FIELD = 5;
+const SQUARES_QTY = 70;
+const INITIAL_TIMER_VALUE = 60;
+const SQUARES_QTY_ON_FIELD = 5;
 
- const GAME_STATUS = {
+const GAME_STATUS = {
   PENDING: 'pending',
   GAME_OVER: 'over',
   GAME_ON: 'on',
   PAUSE: 'pause',
 };
 
- const defaultScoreList = [
+const defaultScoreList = [
   { name: 'Unknown', score: 120 },
   { name: 'John Doe', score: 113 },
   { name: 'Black_Fox', score: 111 },
 ];
 
- const activeClasses = {
+const activeClasses = {
   BLUE: 'square-blue',
   GREEN: 'square-green',
   RED: 'square-red',
@@ -40,18 +42,26 @@ let isOpenModal = false;
 let gameStatus = GAME_STATUS.PENDING;
 let timerValue = INITIAL_TIMER_VALUE;
 let scoreValue = 0;
-let squaresIdList = getRandomNumbers(SQUARES_QTY,SQUARES_QTY_ON_FIELD);
-const scoreList = JSON.parse(localStorage.getItem('score_list')) || defaultScoreList;
+let squaresIdList = getRandomNumbers(SQUARES_QTY, SQUARES_QTY_ON_FIELD);
+const scoreList =
+  JSON.parse(localStorage.getItem('score_list')) || defaultScoreList;
+let intervalId;
 
 //HELPERS
+function formattedTimeValue(sec) {
+  const minutes = Math.trunc(sec / 60);
+  const seconds = sec - minutes * 60;
+  return `0${minutes} : ${seconds > 9 ? seconds : '0' + seconds}`;
+}
+
 const activeClassesList = Object.values(activeClasses);
 
- function setActiveClassForElem (elem) {
+function setActiveClassForElem(elem) {
   const randomIdx = getRandomNumbers(activeClassesList.length);
   return elem.classList.add(activeClassesList[randomIdx]);
-};
+}
 
-function resetActiveClassForElem (elem) {
+function resetActiveClassForElem(elem) {
   switch (true) {
     case elem.classList.contains(activeClasses.GREEN):
       elem.classList.remove(activeClasses.GREEN);
@@ -65,23 +75,23 @@ function resetActiveClassForElem (elem) {
     default:
       return;
   }
-};
+}
 
-function setActiveClassForElements (IdList, parentRef){
+function setActiveClassForElements(IdList, parentRef) {
   for (let item of IdList) {
     const elem = parentRef.children[item.toString()];
     setActiveClassForElem(elem);
   }
-};
+}
 
-function resetActiveClassForElements  (IdList, parentRef) {
+function resetActiveClassForElements(IdList, parentRef) {
   for (let item of IdList) {
-    const elem = parentRef.childNodes[item.toString()];
+    const elem = parentRef.children[item.toString()];
     resetActiveClassForElem(elem);
   }
-};
+}
 
- function getRandomNumbers (max, quantity = 1) {
+function getRandomNumbers(max, quantity = 1) {
   if (quantity === 1) {
     return Math.floor(Math.random() * Math.floor(max));
   } else if (quantity > 1) {
@@ -93,9 +103,9 @@ function resetActiveClassForElements  (IdList, parentRef) {
     }
     return arrOfNumbers;
   } else return;
-};
+}
 
- function getUniqNumber  (primaryList, additionalList = [])  {
+function getUniqNumber(primaryList, additionalList = []) {
   let uniqNumber = getRandomNumbers(SQUARES_QTY, 1);
   if (additionalList.length) {
     if (additionalList.indexOf(uniqNumber) > -1) {
@@ -105,9 +115,9 @@ function resetActiveClassForElements  (IdList, parentRef) {
   if (primaryList.indexOf(uniqNumber) === -1 && uniqNumber !== 0) {
     return uniqNumber;
   } else return getUniqNumber(primaryList, additionalList);
-};
+}
 
- function getUniqNumbersList  (primaryList, withoutZero = false)  {
+function getUniqNumbersList(primaryList, withoutZero = false) {
   const randomQuantity = getRandomNumbers(3, 1);
   if (withoutZero && randomQuantity === 0) {
     return getUniqNumbersList(primaryList, withoutZero);
@@ -123,16 +133,14 @@ function resetActiveClassForElements  (IdList, parentRef) {
     }
     return arrOfUniqNumbers;
   }
-};
+}
 
-
-
-// Inerface upadaters
+// Inerface updaters
 function updateTimerInterface(value) {
-  timerValueElem.innerHTML= value
+  refs.timerValueElem.innerHTML = formattedTimeValue(value);
 }
 function updateScoreInterface(value) {
-  scoreValueElem.innerHTML= value
+  refs.scoreValueElem.innerHTML = value;
 }
 
 // LOGIC
@@ -140,51 +148,55 @@ const handleSetTimerValue = () => {
   timerValue -= 1;
 };
 
-
 const handleStartGame = () => {
   if (gameStatus === GAME_STATUS.GAME_OVER) return;
   if (timerValue === INITIAL_TIMER_VALUE || gameStatus === GAME_STATUS.PAUSE) {
     const prevStatusGame = gameStatus;
     gameStatus = GAME_STATUS.GAME_ON;
-    const intervalId = setInterval(() => {
+    intervalId = setInterval(() => {
       handleSetTimerValue();
-      updateTimerInterface(timerValue)
-      if(squaresIdList.length <= 1){
+      updateTimerInterface(timerValue);
+      if (squaresIdList.length <= 1) {
         const newRandomNumbers = getUniqNumbersList(squaresIdList);
         squaresIdList.push(...newRandomNumbers);
-        console.log("ADDITIONAL SQUARE")
-        setActiveClassForElements(newRandomNumbers, squaresListElem);
+        console.log('ADDITIONAL SQUARE');
+        setActiveClassForElements(newRandomNumbers, refs.squaresListElem);
       }
-      if(timerValue === 0){
+      if (timerValue === 0) {
         gameStatus = GAME_STATUS.GAME_OVER;
         clearInterval(intervalId);
-        formScoreElem.innerHTML = scoreValue;
-        modalElem.classList.remove('modal-closed');
+        refs.formScoreElem.innerHTML = scoreValue;
+        refs.modalElem.classList.remove('modal-closed');
       }
     }, 1000);
     if (prevStatusGame === GAME_STATUS.PENDING) {
-      setActiveClassForElements(squaresIdList, squaresListElem);
+      setActiveClassForElements(squaresIdList, refs.squaresListElem);
     }
   } else {
-   gameStatus = GAME_STATUS.PAUSE;
+    gameStatus = GAME_STATUS.PAUSE;
   }
 };
 
 const handleStartNewGame = () => {
   if (timerValue === INITIAL_TIMER_VALUE) return;
+  if (timerValue !== 0) {
+    clearInterval(intervalId);
+  }
   gameStatus = GAME_STATUS.PENDING;
   scoreValue = 0;
+  updateScoreInterface(scoreValue);
   timerValue = INITIAL_TIMER_VALUE;
-  resetActiveClassForElements(squaresIdList, squaresListElem);
-  setSquaresIdList(getRandomNumbers(SQUARES_QTY, SQUARES_QTY_ON_FIELD));
+  updateTimerInterface(timerValue);
+  resetActiveClassForElements(squaresIdList, refs.squaresListElem);
+  squaresIdList = getRandomNumbers(SQUARES_QTY, SQUARES_QTY_ON_FIELD);
 };
 
 const handleClickOnSquare = e => {
   const elemId = e.target.id;
   const isSquareActive = squaresIdList.indexOf(Number(elemId)) > -1;
   if (gameStatus === GAME_STATUS.GAME_ON && isSquareActive) {
-    scoreValue +=1;
-    updateScoreInterface(scoreValue)
+    scoreValue += 1;
+    updateScoreInterface(scoreValue);
     const newRandomNumbers = getUniqNumbersList(squaresIdList);
     e.target.classList.add('hinge');
     setTimeout(() => {
@@ -195,7 +207,7 @@ const handleClickOnSquare = e => {
     squaresIdList = [...squaresIdList.filter(item => item !== Number(elemId))];
     if (newRandomNumbers.length) {
       squaresIdList.push(...newRandomNumbers);
-      setActiveClassForElements(newRandomNumbers, squaresListElem);
+      setActiveClassForElements(newRandomNumbers, refs.squaresListElem);
     }
   }
 };
@@ -203,22 +215,29 @@ const handleClickOnSquare = e => {
 const handleSubmitResultForm = e => {
   e.preventDefault();
   const nameTargetValue = e.target[0].value;
-  const data = { name: nameTargetValue.length ? nameTargetValue : 'Unknown', score: scoreValue };
-  if(scoreList.length >= 10) {
-    scoreList.pop()
+  const data = {
+    name: nameTargetValue.length ? nameTargetValue : 'Unknown',
+    score: scoreValue,
+  };
+  if (scoreList.length >= 10) {
+    scoreList.pop();
   }
   scoreList.push(data);
-  scoreList.sort((a,b)=> b.score - a.score);
+  scoreList.sort((a, b) => b.score - a.score);
   localStorage.setItem('score_list', JSON.stringify(scoreList));
-  scoreListElem.innerHTML = scoreList.map(item=>`<li class='score-table__item'>${item.name} : ${item.score}</li>`)
-  modalElem.classList.add('modal-closed');
+  refs.scoreListElem.innerHTML = scoreList.map(
+    item => `<li class='score-table__item'>${item.name} : ${item.score}</li>`,
+  );
+  refs.modalElem.classList.add('modal-closed');
 };
 
 // EVENT LISTENERS
-document.addEventListener('DOMContentLoaded',()=> scoreListElem.innerHTML = scoreList.map(item=>`<li class='score-table__item'>${item.name} : ${item.score}</li>`))
-startButtonElem.addEventListener('click', handleStartGame);
-newGameButtonElem.addEventListener('click',handleStartNewGame );
-squaresListElem.addEventListener('click', handleClickOnSquare);
-resultFormElem.addEventListener('submit',handleSubmitResultForm);
-
-/// сделать загрузку очков при бутстрапе
+document.addEventListener('DOMContentLoaded', () => {
+  refs.scoreListElem.innerHTML = scoreList.map(
+    item => `<li class='score-table__item'>${item.name} : ${item.score}</li>`,
+  );
+});
+refs.startButtonElem.addEventListener('click', handleStartGame);
+refs.newGameButtonElem.addEventListener('click', handleStartNewGame);
+refs.squaresListElem.addEventListener('click', handleClickOnSquare);
+refs.resultFormElem.addEventListener('submit', handleSubmitResultForm);
