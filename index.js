f320562d0f3e75a7e68577fc30e3e96c6a23ca50
Stanislav <stastaransa1nt@gmail.com>
2021-01-03 19:00:33 +0200

    add cheking file exists

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 const express = require('express');
+const fs = require('fs');
 const serveStatic = require('serve-static');
 const { resultsRouter } = require('./modules/results/results.router');
 
 const PORT = process.env.PORT || 3000;
 
+const staticServehandler = serveStatic('public', {
+  index: ['index.html', 'index.htm'],
+  extensions: ['html', 'htm'],
+});
+
 try {
   const app = express();
 
-  app.use(serveStatic('public', { index: ['index.html', 'index.htm'] }));
+  app.use(staticServehandler);
+
+  app.use((req, res, next) => {
+    if (req.url.split('/')[1] === 'api') {
+      next();
+    } else {
+      return res.status(404).sendFile('public/_404.html', { root: __dirname });
+    }
+  });
 
   app.use(express.json());
 
   app.use('/api/results', resultsRouter);
 
   app.listen(PORT, () =>
-    console.log(`\x1B[33mServer listening on port: 3000... \x1b[0m`),
+    console.log(`\x1B[35mServer listening on port: 3000... \x1b[0m`),
   );
 } catch (e) {
   console.log(e);
