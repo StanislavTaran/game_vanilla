71f1aecf6672f18d761b6e925ace68458536930c
Stanislav <stastaransa1nt@gmail.com>
2021-01-04 11:22:18 +0200

    check result data from form

--- public/main.js
+++ public/main.js
@@ -9,11 +9,12 @@ const refs = {
   formScoreElem: document.getElementById('form-score'),
   resultFormElem: document.getElementById('result-form'),
   modalElem: document.getElementById('modal'),
+  formStatusElem: document.getElementById('form-status'),
 };
 
 //CONSTANTS
 const SQUARES_QTY = 70;
-const INITIAL_TIMER_VALUE = 60;
+const INITIAL_TIMER_VALUE = 6;
 const SQUARES_QTY_ON_FIELD = 5;
 
 const GAME_STATUS = {
@@ -164,6 +165,10 @@ function updateScoreInterface(value) {
   refs.scoreValueElem.innerHTML = value;
 }
 
+function updateFormStatus(str) {
+  refs.formStatusElem.innerHTML = str
+}
+
 // LOGIC & EVENT HANDLERS
 const handleSetTimerValue = () => {
   gameState.timerValue -= 1;
@@ -245,12 +250,17 @@ const handleSubmitResultForm = async e => {
     score: gameState.scoreValue,
   };
   try {
-    await postResult(data);
-    await getRemoteResultsAndRender();
+    const response = await postResult(data);
+    const result = await response.json();
+
+    if(result.error){
+      updateFormStatus(result.message)
+    } else {
+      await getRemoteResultsAndRender();
+      refs.modalElem.classList.add('modal-closed');
+    }
   } catch (e) {
     console.log(e);
-  } finally {
-    refs.modalElem.classList.add('modal-closed');
   }
 };
 
